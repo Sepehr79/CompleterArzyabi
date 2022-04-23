59f63981bc6a1f90a7f878a3a836e28c0582a701
Sepehr79 <sepehrmsm1379@gmail.com>
2022-04-23 17:58:26 +0430

    using config parser

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# Run with docker
+# CompleterArzyabi
+پر کردن خودکار فرم ارزیابی اساتید دانشگاه دولتی کاشان
 
-`docker run -it -e USER=(your username) -e PASSWORD=(your password) -e SALARY=(نمره استاد) analyze_ostad`
+[See on Instagram](https://www.instagram.com/p/BsgN-iTA2Xqx1A8kG92amRlf3PXqGDVk3EDNxs0/)
+
+برای اجرا یادتون نره کتابخانه سلنیوم رو نصب کنید و اگر نیاز به وب درایور داشتید براتون در سورس قرار دادم
--- formeArzyabiAsatid.py
+++ formeArzyabiAsatid.py
@@ -3,30 +3,19 @@
 @edited for kashanu ac: mrunderline, Sepehr79
 """
 
-from cmath import log
+import configparser
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.ui import Select
-import time
-import os
 
-# Get environment variable values
-userName = os.getenv("USER")
-password = os.getenv("PASSWORD")
-salary = os.getenv("SALARY")
+parser = configparser.ConfigParser()
+parser.read("properties.ini")
 
-# Chrome options
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_prefs = {}
-chrome_options.experimental_options["prefs"] = chrome_prefs
-chrome_prefs["profile.default_content_settings"] = {"images": 2}
+username = parser.get("SETTINGS", "username")
+password = parser.get("SETTINGS", "password")
+ostad_score = parser.get("SETTINGS", "ostad_score")
 
 # Chrome driver
-driver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Chrome('./chromedriver.exe')
 driver.maximize_window()
 
 def get_teachers():
@@ -36,7 +25,7 @@ def get_teachers():
 def login():
 	url = 'https://pooya.kashanu.ac.ir/gateway/PuyaAuthenticate.php'
 	driver.get(url)
-	driver.find_element_by_id('UserID').send_keys(userName) 
+	driver.find_element_by_id('UserID').send_keys(username) 
 	driver.find_element_by_id('DummyVar').send_keys(password)
 	driver.find_element_by_xpath(".//input[@type='submit']").click()
 	print("loged in")
@@ -51,7 +40,7 @@ while verify < teachers_count:
 
 	for select in driver.find_elements_by_tag_name('select'):
 		select = Select(select)
-		select.select_by_value(salary)
+		select.select_by_value(ostad_score)
 	driver.find_element_by_id("FSubmit").click()
 	driver.switch_to_alert().accept()
 	print ('teacher {} verified!'.format(verify))
